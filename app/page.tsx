8ea53bdd0af3998d364a00be8cdcd187cbec9054
heyongqi10 <heyongqi10@jd.com>
2025-08-09 22:24:29 +0800

    feat: spilt the screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import CodeEditor from "@/components/CodeEditor";
 
 export default function Home() {
   return (
-    <div className="font-sans">
-      <header className="h-20 bg-gray-100 border-b border-gray-200">rslint playground</header>
-      <main>
-        <section>
-          <section>
+    <div className="font-sans w-full h-full flex flex-col">
+      <header className="h-20 bg-gray-100 border-b border-gray-200 shrink-0">rslint playground</header>
+      <main className="flex-1 grid grid-cols-2">
+        <section className="border-r border-gray-200 grid grid-rows-2">
+          <section className="border-b border-gray-200">
             <CodeEditor />
           </section>
-          <section>
+          <section className="border-t border-gray-200">
             Options
           </section>
         </section>
-        <section>
+        <section className="border-l border-gray-200">
           MessageBox
         </section>
       </main>
